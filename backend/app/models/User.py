d18e2019b8899ceb963ca73c581a74bfb75be023
Hantuna8459 <55192795+Hantuna8459@users.noreply.github.com>
2024-10-09 09:23:09 +0700

    add first name, last name

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -14,7 +14,8 @@ class User(BaseModel):
     username = Column("name", String(30), nullable=False, unique=True)
     password = Column("password", String(50), nullable=False)
     email = Column("email", String(50), nullable=False, unique=True)
-    full_name = Column("full_name", String(255), default=None, nullable=True)
+    first_name = Column("first_name", String(50), default=None, nullable=True)
+    last_name = Column("last_name", String(50), default=None, nullable=True)
     last_login = Column("last_login", DateTime,default=datetime.now(), nullable=True)
     is_active = Column("is_active", Boolean, default=True)
     
