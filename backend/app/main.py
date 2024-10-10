80a1306c8904d2f3c6fafcfee7e6552d087c4e96
Phan Tien Tai <taip7547@gmail.com>
2024-10-10 14:40:32 +0700

    fix endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 # app run here
 from fastapi import FastAPI
 from backend.app.core.config import settings
+from backend.app.api.routes import auth_router
 
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
+
+app.include_router(auth_router, tags=["auth"])
