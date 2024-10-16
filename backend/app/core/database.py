68acc0fc14ffbae280bbe2a6618aadb650168ef2
Phan Tien Tai <taip7547@gmail.com>
2024-10-16 22:23:14 +0700

    Async Session and DBExecuteException

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,20 +1,23 @@
 # db connect here
-from typing import Generator
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.schema import MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
-from core.config import settings
+from .config import settings
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base(metadata=MetaData())
 
-def get_db() -> Generator:
+def get_db() -> AsyncSession:
     try:
         db = SessionLocal()
         yield db
     finally:
         db.close()
+
+class DatabaseExecutionException(Exception):
+    def __init__(self, message: str):
+        self.message = f"Error executing SQL: {message}"
+        super().__init__(self.message)
