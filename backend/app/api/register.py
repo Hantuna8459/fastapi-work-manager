4656d6bcdc1285b4a6a81ba798e40c4e47b4758b
Hantuna8459 <55192795+Hantuna8459@users.noreply.github.com>
2024-10-21 14:55:40 +0700

    separate validation

--- backend/app/api/register.py
+++ backend/app/api/register.py
@@ -1,7 +1,5 @@
 from typing import Any
 from fastapi import APIRouter, Depends
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from backend.app.utils import generate_register_mail, send_mail
 from backend.app.crud import user_crud
 from backend.app.schema.user_schema import UserRegisterRequest, UserResponse
@@ -15,37 +13,39 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
-@router.post("/signup", response_model=UserResponse)
-async def register_user(*, user_in:UserRegisterRequest,
-                        session:AsyncSession = Depends(get_db))->Any:
-
-    try:
-        # check if email or username already existed
-        existed_user = await user_crud.get_user_by_email_or_username(
+async def validate_user_data(session:AsyncSession, user_in:UserRegisterRequest):
+    # check if email or username already registed
+    existed_user = await user_crud.get_user_by_email_or_username(
             session=session,
             email=user_in.email,
             username=user_in.username
             )
-        if existed_user:
-            if existed_user.email == user_in.email:
-                raise EmailDuplicateException
-            if existed_user.username == user_in.username:
-                raise UsernameDuplicateException
+    if existed_user:
+        if existed_user.email == user_in.email:
+            raise EmailDuplicateException
+        if existed_user.username == user_in.username:
+            raise UsernameDuplicateException
 
-        #TO DO: move validate out of endpoint
-        if not user_in.password_confirm == user_in.password:
-            raise UnmatchedPasswordException
+    # check unmatched password
+    if not user_in.password_confirm == user_in.password:
+        raise UnmatchedPasswordException
+    
 
-        user_data = UserRegisterRequest.model_validate(user_in)
-        new_user = await user_crud.register_request(session=session, request=user_data)
+@router.post("/signup", response_model=UserResponse)
+async def register_user(*, user_in:UserRegisterRequest,
+                        session:AsyncSession = Depends(get_db))->Any:
 
-        # send email
+    await validate_user_data(session, user_in)
+    
+    user_data = UserRegisterRequest.model_validate(user_in)
+    new_user = await user_crud.register_request(session=session, request=user_data)
+
+    # send email
+    if new_user:
         email_data = generate_register_mail(email_to=user_in.email, username=user_in.username)
         send_mail(
             email_to=user_in.email,
             subject=email_data.subject,
             html_content=email_data.html_content,
         )
-    except Exception as e:
-        raise e
-    return JSONResponse(jsonable_encoder({"msg": "User registered successfully", "user": new_user}))
+    return new_user
