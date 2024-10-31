4fb3914ee42f0c4a07f05b012572ea2e49102c13
Phan Tien Tai <taip7547@gmail.com>
2024-10-31 09:43:16 +0700

    check blank of TodoItem and Category create schema

--- backend/app/schema/todo_item.py
+++ backend/app/schema/todo_item.py
@@ -1,12 +1,34 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class TodoItemBaseSchema(BaseModel):
     name: str
     description: str
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+
+        if name == "":
+            raise ValueError("Name can't be blank")
+
+        return name
+
+    @field_validator("description")
+    @classmethod
+    def validate_name(cls, description):
+        if not isinstance(description, str):
+            raise ValueError("Description must be a string")
+
+        if description == "":
+            raise ValueError("Description can't be blank")
+
+        return description
 
 
 class TodoItemWithCategorySchema(TodoItemBaseSchema):
